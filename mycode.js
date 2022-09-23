1f20f5a15e4e42927c775f80a727e3a32828846d
Vadata <dutaen1011@gmail.com>
2022-09-23 22:37:05 +0700

    Push Portfolio 1 time

---
+++ mycode.js
@@ -0,0 +1,8 @@
+function Contact() {
+  var phone = prompt("Enter your numberphone(11): ");
+  if (phone.length == 11) {
+    alert("we will messing for you soon, please enter OPT right here: ");
+  } else {
+    alert("Number does not exist");
+  }
+}
